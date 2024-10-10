884748e90222923d5543f220b76cb4a8a3f22b99
Jayki-ZX <danteantu5@gmail.com>
2024-10-09 23:35:11 -0300

    dummy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { AComponent } from './a/a.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent,
+        children: [{
+            path: '/a',
+            component: AComponent
+        }]
+    },
+    {
+        path: '/new_component',
+        component: NewComponentComponent
+    },
+    {
+        path: '**',
+        component: AppComponent
+    }
+];
